9052a179da0a2a830e99944ccaa2443e0ac8ece8
Iryna <batsianiryna@gmail.com>
2022-11-26 23:41:22 +0300

    add task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+
+Console.Write("Введите цифру: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 1 || n == 2 || n == 3 || n == 4 || n == 5)
+ Console.Write("NO");
+else if (n == 6 || n == 7)
+  Console.Write("YES");
+else
+  Console.Write("Неверно введена цифра");
+
